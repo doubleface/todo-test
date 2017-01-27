4c7efcb132a8e837ef6ca30aab383d1ab3064be6
doubleface <mr.thiriot@gmail.com>
2017-01-27 13:54:50 +0100

    todos creation

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const todo = document.querySelector("#todo");
 
 let plist = cozysdk.queryView("tasky", "all");
 
-function displayTodo(list) {
+function createTodos() {
+    var p = Promise.resolve();
+    for (let i = 0; i< 10; i++) {
+        p.then(() => {
+            return cozysdk.create("tasky", {description: `new task ${i}`});
+        });
+    }
+
+    p.then(displayTodos);
+}
+function displayTodos(list) {
     todo.innerHTML = list.map(item => {
         return `<li>${item.value.description}</li>`;
     }).join("\n");
 }
 
-plist.then(displayTodo);
+plist.then(displayTodos);
+
+window.createTodos = createTodos;
