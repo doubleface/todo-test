e3458b1341629930f88c3e74d3ddba316371b246
doubleface <mr.thiriot@gmail.com>
2017-01-30 15:38:49 +0100

    Added bootstrap styling

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const todo = document.querySelector("#todo");
 
 function displayTodos() {
     window.cozysdk.queryView("tododemo", "all")
+    /* Promise.resolve([
+        {value: {description: "toto"}},
+        {value: {description: "bonjour tout le monde"}},
+        {value: {description: "Voir ce que ça donne avec bootstrap"}},
+    ]) */
     .then(list => {
         todo.innerHTML = list.map(item => {
-            return `<li>${item.value.description}</li>`;
+            return `<li class="list-group-item">${item.value.description}</li>`;
         }).join("");
     });
 }
