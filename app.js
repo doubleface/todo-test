ccd0000a6074abb6cac345c6ea8519c65923d184
doubleface <mr.thiriot@gmail.com>
2017-01-31 13:51:10 +0100

    finally removed checkbox

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const todo = document.querySelector("#todo");
 
-function itemTemplate(item){
-    return `<li><input type="checkbox" ${item.checked ? "checked" : ""} /> ${item.description}</li>`;
-}
-
 function displayTodos() {
-    cozysdk.queryView("tododemo", "all")
+    window.cozysdk.queryView("tododemo", "all")
     .then(list => {
         todo.innerHTML = list.map(item => {
-            return itemTemplate(item.value);
+            return `<li>${item.value.description}</li>`;
         }).join("");
     });
 }
